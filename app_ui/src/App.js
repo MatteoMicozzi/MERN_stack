dffbb44201b4352b3ab2a10c741997b319bb19ae
MatteoMicozzi <micozzimatteo@yahoo.it>
2022-07-20 21:20:10 +0100

    Created Path Routes with components.

--- app_ui/src/App.js
+++ app_ui/src/App.js
@@ -7,5 +7,19 @@ import ShowBookList from './components/ShowBookList';
 import ShowBookDetails from './components/ShowBookDetails';
 import UpdateBookInfo from './components/UpdateBookInfo';
 
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowBookList} />
+          <Route path='/create-book' component={CreateBook} />
+          <Route path='/edit-book/:id' component={UpdateBookInfo} />
+          <Route path='/show-book/:id' component={ShowBookDetails} />
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
